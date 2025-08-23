bf3762392e0c3b948df592f1a34881c16d660b78
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-08-23 10:00:08 +0200

    fixed code

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-let forCase;
+const numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-if (numOrStr === null) {
-    forCase = 'скасовування';
-} else if (numOrStr.trim() === '') {
-    forCase = 'Empty String';
-} else if (isNaN(+numOrStr)) {
-    forCase = 'number is Ba_NaN';
-} else {
-    forCase = 'OK!';
-}
-
-
-switch (forCase) {
-    case 'скасовування':
+switch (true) {
+    case (numOrStr === null):
         console.log('ви скасували');
         break;
-    case 'Empty String':
+
+    case (numOrStr.trim() === ''):
         console.log('Empty String');
         break;
-    case 'number is Ba_NaN':
-        console.log(' number is Ba_NaN');
-        break;             
-    case 'OK!':
-        console.log('OK!')
+
+    case (isNaN(+numOrStr)):
+        console.log('number is Ba_NaN');
+        break;
+
+    default:
+        console.log('OK!');
         break;
 }
